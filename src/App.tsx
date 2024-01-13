f6890ea4627bb6dbc28711659d66e15e557dd6f9
DESKTOP-RLUVES5\Maedeh Tehrani <maede.alirezaei@gmail.com>
2024-01-13 15:28:50 +0100

    init project and openlayers lib

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MapView from "./components/MapView";
+
+function App() {
+  return (
+    <div style={{ width: "100%", height: "400px" }}>
+      <MapView />
+    </div>
+  );
+}
+
+export default App;
